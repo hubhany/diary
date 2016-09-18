d783a7a924b7be501ad3e2f4fc1dad5ad53b5dbb
cd C:\RailsInstaller\Ruby2.2.0\bin <copy rake.bat rails.bat>
2016-09-18 15:33:17 +0200

    first commit

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,36 @@
+class EntriesController < ApplicationController
+  def index
+    @entries = Entry.all
+    @websites = [
+     ["http://railsgirls.com", "Rails Girls"],
+     ["https://en.wikibooks.org/wiki/Ruby_Programming", "Wikibooks"],
+     ["http://guides.rubyonrails.org", "Ruby on Rails Guides"],
+    ]
+  end
+
+  def show
+    @entry = Entry.find(params["id"])
+  end
+
+  def new
+
+  end
+
+  def create
+    entry_params = params["entry"].permit("title", "contents")
+    entry = Entry.create(entry_params)
+    redirect_to(entry_path(entry))
+  end
+
+  def edit
+    @entry = Entry.find(params["id"])
+  end
+  
+  def update
+  entry_params = params["entry"].permit("title", "contents")
+  entry = Entry.find(params["id"])
+  entry.update(entry_params)
+  redirect_to(entry_path(entry))
+  end
+
+end
